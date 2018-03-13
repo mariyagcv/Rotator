60f1517cc4e73da8cf6395efbc147f53cbcfbb5c
hindolo <hindolo@btinternet.com>
2018-03-13 02:44:04 +0000

    progress on login scripts but i need a uni ip (ethernet cable at accomodation) to properly test and continue

--- php/loginscripts/hash.php
+++ php/loginscripts/hash.php
@@ -3,7 +3,7 @@
 
 //function to convert plaintext passwords to hashed values stored in database
 //default value of pasword to return is empty string if not given password
-function hash($pword = ""){
+function pwHash($pword = ""){
 
   //currently only returns the password it was given
   
--- php/loginscripts/sqlselect.php
+++ php/loginscripts/sqlselect.php
@@ -2,7 +2,7 @@
 //class to send sql select queries to database and return info
 
 
-class database
+class Database
 {
 
   //function to get data from a table in the database
@@ -42,6 +42,40 @@ class database
     //return the values (or null) retrieved from select query
     return $row;
 
+  }
+
+  function select($field,$table,$condField,$condValue){
+  
+    //create the database connection object
+    $dbobj = new mysqli("dbhost.cs.man.ac.uk","p34897st","databasepa5s","2017_comp10120_z8");
+
+    //check if connection worked without errors, and cancel if there was
+    if($dbobj -> connect_error) {
+      die('Connect Error ('.$dbobj -> connect_errno.') '.$dbobj -> connect_error);
+    }
+
+
+    //sanitise inputs
+    $field = $dbobj -> real_escape_string($field);
+    $table = $dbobj -> real_escape_string($table);
+    
+
+    //set up array for retrieved outputs
+    
+    $row = null;
+
+    //select values from table with condition applied
+    if($result = $dbobj -> query("SELECT ".$field." FROM ".$table." WHERE ".$condField."=".$condValue)) {
+      
+      //save array of values to return
+      $row = $result -> fetch_array(MYSQLI_NUM);
+
+      $result -> close(); // Remember to release the result set
+      $dbobj -> close(); //close db object
+    }
+
+    //return the values (or null) retrieved from select query
+    return $row;
   }
 }
 ?>
