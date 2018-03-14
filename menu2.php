b9fa6040e66c34d3f1f83a306b2f9b3a5a9f93bb
hindolo <hindolo@btinternet.com>
2018-03-14 11:09:48 +0000

    finished menu but it works with php files instead of html files

--- menu2.php
+++ menu2.php
@@ -1,10 +1,9 @@
 <?php
 
- <ul>
-  <li><a href="timetable.html">Timetable</a></li>
-  <li><a href="newsfeed.html">Newsfeed</a></li>
-  <li><a href="settings.html">Settings</a></li>
-  <li><a href="about.html">About</a></li>
-</ul> 
-
+echo " <ul>";
+echo " <li><a href=\"timetable.html\">Timetable</a></li>";
+echo "  <li><a href=\"newsfeed.html\">Newsfeed</a></li>";
+echo "  <li><a href=\"settings.html\">Settings</a></li>";
+echo "  <li><a href=\"about.html\">About</a></li>";
+echo " </ul>";
 ?>
