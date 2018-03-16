72239ce0269484388af8da6521d1f2e8192aacca
Alejandro <alejandro.rodriguez@student.manchester.ac.uk>
2018-03-16 13:21:20 +0000

    classes added to all paragraphs

--- newsfeed.php
+++ newsfeed.php
@@ -10,8 +10,8 @@
   <h1 class = "taskClass">News Feed</h1>
   <div class = "tasks"><p class = "taskList">Alexa cleaned the dishes on 04/03/18 at 18:14</p>
     <p class = "taskList">Lucia threw out the trash on 03/03/18 at 10:15</p>
-    <p>Claudia cleaned the kitchen surfaces on 02/03/18 at 19:12</p>
-    <p>Juan please verify Lucia's task of throwing out the trash</p>
+    <p class = "taskList">Claudia cleaned the kitchen surfaces on 02/03/18 at 19:12</p>
+    <p class = "taskList">Juan please verify Lucia's task of throwing out the trash</p>
     </div>
   <?php include 'menu2.php';?>
 </body>
