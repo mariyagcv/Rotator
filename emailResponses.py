562ce16cce22fcbd6c92aeb40b489acaf8793692
Daria Tolmacheva <daria.tolmacheva@student.manchester.ac.uk>
2018-03-19 20:51:50 +0000

    daily notification funcrion is fixed

--- emailResponses.py
+++ emailResponses.py
@@ -19,6 +19,10 @@ from oauth2client import tools
 from oauth2client.file import Storage
 
 import mysql.connector
+from datetime import timedelta, date, datetime
+
+global connection
+global cursor
 
 try:
     import argparse
@@ -35,31 +39,31 @@ SCOPES = 'https://www.googleapis.com/auth/gmail.compose'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Gmail API Python Quickstart'
 
-
-#CHECKED
+# NOT USED ANYMOE: WE CHANGED THE APROACH
+# CHECKED
 # Method that creates drafts for further reminder emails.
 # Should be used when tasks are allocated.
 # Parameters: name of the reciever,
 #             email of the reciever,
 #
-def mailResponseToAssignment(name, email): #taskID):
+#def mailResponseToAssignment(name, email): #taskID):
 
   
 
   # create draft for one day prior notification
-  oneDayPriorSubject = "Tomorrow's Deadline"
-  oneDayPriorText = "Dear %s!\nYour task deadline is tomorrow. For more info log in to your Rotator account." % name
-  oneDayPriorTextID = create_draft("me", create_message(email, oneDayPriorSubject, oneDayPriorText))
+#  oneDayPriorSubject = "Tomorrow's Deadline"
+#  oneDayPriorText = "Dear %s!\nYour task deadline is tomorrow. For more info log in to your Rotator account." % name
+#  oneDayPriorTextID = create_draft("me", create_message(email, oneDayPriorSubject, oneDayPriorText))
 
   # create draft for day of deadline notification
-  deadlineDaySubject = "Today's Deadline"
-  deadlineDayText = "Dear %s!\nYour task deadline is today.\nMake sure You copmlete it in time.\nFor more info log in to your Rotator account." % name
-  deadlineDayTextID = create_draft("me", create_message(email, deadlineDaySubject, deadlineDayText))
+#  deadlineDaySubject = "Today's Deadline"
+#  deadlineDayText = "Dear %s!\nYour task deadline is today.\nMake sure You copmlete it in time.\nFor more info log in to your Rotator account." % name
+#  deadlineDayTextID = create_draft("me", create_message(email, deadlineDaySubject, deadlineDayText))
 
   # create draft for one day late notification
-  oneDayAfterSubject = "Missed Deadline"
-  oneDayAfterText = "Dear %s!\nYour task deadline was yesterday. For more info log in to your Rotator account." % name
-  oneDayAfterTextID = create_draft("me", create_message(email, oneDayAfterSubject, oneDayAfterText))
+#  oneDayAfterSubject = "Missed Deadline"
+#  oneDayAfterText = "Dear %s!\nYour task deadline was yesterday. For more info log in to your Rotator account." % name
+#  oneDayAfterTextID = create_draft("me", create_message(email, oneDayAfterSubject, oneDayAfterText))
 
   #for testing
   #draft_ids = []
@@ -68,10 +72,10 @@ def mailResponseToAssignment(name, email): #taskID):
   #draft_ids.append(oneDayAfterTextID)
   #return draft_ids
 
-  # !!! PUT IDS TO THE DATABASE !!!
 
 
-# CHECKED APART FROM FREEZING
+
+# CHECKED
 # Method that sends requests to verify the submition
 # to other members of the group and freezes reminder drafts.
 # Should be used when task is submitted.
@@ -86,9 +90,9 @@ def mailResponseToSubmit(emails, draftIDs):
   service = discovery.build('gmail', 'v1', http=http)
 
   # freeze notifications for this task
-  if (len(draftIDs) != 0):
-    for i in range(0, len(draftIDs) - 1):
-      draftIDs[i]
+#  if (len(draftIDs) != 0):
+#    for i in range(0, len(draftIDs) - 1):
+#      draftIDs[i]
       # CHANGE 'SUBMITTED' FLAG IN THE DATABASE
 
   # send verification emails
@@ -104,7 +108,7 @@ def mailResponseToSubmit(emails, draftIDs):
 # whose task was verified, and deletes the reminder drafts.
 # Should be used when task is verified.
 # Parameters: array of draft ids associated with the task verified,
-def mailResponseToVerify(draftIDs):
+def mailResponseToVerify(task_ID):
   #
   #
   # some needed housekeeping
@@ -112,37 +116,68 @@ def mailResponseToVerify(draftIDs):
   http = credentials.authorize(httplib2.Http())
   service = discovery.build('gmail', 'v1', http=http)
 
-  # delete notifications for this task
-  if (len(draftIDs) != 0):
-    for i in range(0, len(draftIDs) - 1):
-      try:
-        service.users().drafts().delete(userId="me", id=draftIDs[i]).execute()
-        # print 'Draft with id: %s deleted successfully.' % draft_id
-      except errors.HttpError, error:
-        print("An error occurred: %s" % error)
-      # DELETE ENTRY IN THE DATABASE
+
+# !!!!! inform the person that their task was verified
 
 
-#CHECKED
 # Method to send timed notifications daily
-# Parameters: array of draft IDs such that
+# Parameters: array of user IDs such that
 #             the task is not submitted and
-#             either deadline = today + 1, type = prior
-#             or     deadline = today,     type = today
-#             or     deadline = today - 1, type = late.
+#             either deadline = today + 1,
+#             or     deadline = today,
+#             or     deadline = today - 1,
 #
-def dailyEmailNotifications(draftsToSend):
+def dailyEmailNotifications():
 
   # some needed housekeeping
   credentials = get_credentials()
   http = credentials.authorize(httplib2.Http())
   service = discovery.build('gmail', 'v1', http=http)
 
-  # send the drafts
-  for i in range(0, len(draftsToSend)):
-    send_draft("me", draftsToSend[i])
-    #DELETE ENTRY FROM THE DATABASE
+  connection = mysql.connector.connect(
+    user="mbyxadr2", database="2017_comp10120_z8", password="fA+h0m5_", host = "dbhost.cs.man.ac.uk"
+    )
+  cursor= connection.cursor(buffered=True)
 
+
+  # send the notifications
+  #calculate day difference (to an hour)
+  cursor.execute("SELECT User_Task_Log.Deadline FROM User_Task_Log WHERE User_Task_Log.Submitted = 0" 
+                         )
+  deadlines = cursor.fetchall()
+  cursor.execute("SELECT User_ID FROM User_Task_Log WHERE User_Task_Log.Submitted = 0" 
+                         )
+  user_ids = cursor.fetchall()
+
+  oneDayPriorSubject = "Tomorrow's Deadline"
+  deadlineDaySubject = "Today's Deadline"
+  oneDayAfterSubject = "Missed Deadline"
+
+  for i in range(0, len(deadlines)):
+    notificationDeadline = timedelta((today - deadlines[0]).days)
+    # tomorrow is deadline
+    if notificationDeadline == -1:
+      cursor.execute("SELECT User.Email, User.Name FROM User WHERE User.ID = %s" % user_ids[i])
+      email =  cursor.fetchall()
+      oneDayPriorText = "Dear %s!\nYour task deadline is tomorrow. For more info log in to your Rotator account." % email[1]
+      oneDayPriorTextID = send_message("me", create_message(email[0], oneDayPriorSubject, oneDayPriorText))
+
+    # today is deadline
+    elif notificationDeadline == 0:
+      cursor.execute("SELECT User.Email, User.Name FROM User WHERE User.ID = %s" % user_ids[i])
+      email =  cursor.fetchall()
+      deadlineDayText = "Dear %s!\nYour task deadline is today.\nMake sure You copmlete it in time.\nFor more info log in to your Rotator account." % email[1]
+      deadlineDayTextID = create_draft("me", create_message(email[0], deadlineDaySubject, deadlineDayText))
+
+      
+    # yesterday was deadline
+    elif notificationDeadline == 1:
+      cursor.execute("SELECT User.Email, User.Name FROM User WHERE User.ID = %s" % user_ids[i])
+      email =  cursor.fetchall()
+      oneDayAfterText = "Dear %s!\nYour task deadline was yesterday. For more info log in to your Rotator account." % email[1]
+      oneDayAfterTextID = create_draft("me", create_message(email[0], oneDayAfterSubject, oneDayAfterText))
+
+      
 
 
 # helper functions
@@ -158,24 +193,24 @@ def create_message(to, subject, message_text):
 
 
 # creates a draft
-def create_draft(user_id, message_body):
+#def create_draft(user_id, message_body):
 
   # some needed housekeeping
-  credentials = get_credentials()
-  http = credentials.authorize(httplib2.Http())
-  service = discovery.build('gmail', 'v1', http=http)
+#  credentials = get_credentials()
+#  http = credentials.authorize(httplib2.Http())
+#  service = discovery.build('gmail', 'v1', http=http)
 
-  try:
-    message = {'message': message_body}
-    draft = service.users().drafts().create(userId=user_id, body=message).execute()
+#  try:
+#    message = {'message': message_body}
+#    draft = service.users().drafts().create(userId=user_id, body=message).execute()
 
     # for testing
     # print 'Draft id: %s\nDraft message: %s' % (draft['id'], draft['message'])
 
-    return draft["id"]
-  except HttpError, error:
-    print('An error occurred: %s' % error)
-    return None
+#    return draft["id"]
+#  except HttpError, error:
+#    print('An error occurred: %s' % error)
+#    return None
 
 # deletes a draft
 
