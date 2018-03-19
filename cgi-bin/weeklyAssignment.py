ad8a4eb0839d289e4d37fb0c46a18ad986177366
Dawid Lorenz <dawid.lorenz@student.manchester.ac.uk>
2018-03-19 14:20:40 +0000

    Replace weeklyAssignment.py

--- cgi-bin/weeklyAssignment.py
+++ cgi-bin/weeklyAssignment.py
@@ -1,13 +1,20 @@
-from assigningTasksWeekly import query
+from assigningTasksWeekly import query, rank
 import mysql.connector
 from datetime import datetime
+from threading import Timer
 
-
-connection = mysql.connector.connect(
-user="mbyxadr2", database="2017_comp10120_z8", password="fA+h0m5_", host = "dbhost.cs.man.ac.uk"
-)
-cursor= connection.cursor(buffered=True)
-cursor.execute("SELECT ID FROM WorkGroup")
-groups = cursor.fetchall()
-for group in groups:
-  query(group[0], datetime.now() )
+def run():
+  connection = mysql.connector.connect(
+  user="mbyxadr2", database="2017_comp10120_z8", password="fA+h0m5_", host = "dbhost.cs.man.ac.uk"
+  )
+  cursor= connection.cursor(buffered=True)
+  cursor.execute("SELECT ID FROM WorkGroup")
+  groups = cursor.fetchall()
+  for group in groups:
+    rank(group[0], datetime.now() )
+    query(group[0], datetime.now() )
+  
+  cursor.close()
+  connection.close()
+    
+schedule.every().monday.at('00:01').do(run)
